2d75aeed117ebd63b2faa1ea2e8bc7ecd9856591
Mithun Balasubramanian <24193159+mithunb9@users.noreply.github.com>
2023-11-12 06:29:31 -0600

    fix background and styling

--- client/pages/study.js
+++ client/pages/study.js
@@ -12,43 +12,39 @@ const Study = () => {
   const router = useRouter();
 
   useEffect(() => {
-    let length = parseInt(router.query.length);
-    let tasks = undefined;
+    if (!router.isReady) return; // Ensure router is ready
 
-    if (router.query.tasks != undefined) {
-      tasks = JSON.parse(router.query.tasks);
-    }
+    let length = parseInt(router.query.length) || 0;
+    let tasks = router.query.tasks ? JSON.parse(router.query.tasks) : [];
 
     console.log(length);
 
-    if (tasks != undefined) {
+    // Check if tasks is defined and is an array
+    if (Array.isArray(tasks)) {
       // sort data based on timeToComplete
-
       tasks.sort(
         (a, b) =>
-          extractHours(a.timeToComplete) - extractHours(b.timeToComplete)
+          extractHours(a?.timeToComplete) - extractHours(b?.timeToComplete)
       );
 
-      // implement 0 - 1  knapsack
+      // implement 0 - 1 knapsack
       let timeTaken = 0;
       let out = [];
 
-      for (let i = 0; i < tasks.length; i++) {
-        let possibleLength = extractHours(tasks[i].timeToComplete);
-        console.log("Calc: ", timeTaken + possibleLength);
-        if (timeTaken + possibleLength <= 4) {
-          length = timeTaken + possibleLength;
-          out.push(tasks[i]);
+      for (let task of tasks) {
+        let possibleLength = extractHours(task?.timeToComplete);
+
+        if (timeTaken + possibleLength <= length) {
+          timeTaken += possibleLength;
+          out.push(task);
         } else {
           break;
         }
       }
 
       setData(out);
-
-      console.log(out);
     }
-  }, []);
+  }, [router.isReady, router.query]);
 
   return (
     <div>
@@ -58,27 +54,29 @@ const Study = () => {
         }
       >
         <Navbar />
-        {data?.map((task) => (
-          <div
-            key={task.name}
-            className="bg-white py-[5%] m-[5%] h-[35%] w-[90%] rounded-xl flex flex-col justify-center items-center shadow-md text-black"
-          >
-            <div className="w-[90%] flex flex-row justify-between">
-              <h1 className="text-2xl  ">{task.name}</h1>
+        <div className="overflow">
+          {data?.map((task) => (
+            <div
+              key={task.name}
+              className="bg-white py-[5%] m-[5%] h-[35%] w-[90%] rounded-xl flex flex-col justify-center items-center shadow-md text-black"
+            >
+              <div className="w-[90%] flex flex-row justify-between">
+                <h1 className="text-2xl  ">{task.name}</h1>
+              </div>
+              <div className="flex flex-row w-[90%] h-[20%] justify-evenly item-center ml-[-5%] gap-[10%] mt-[10%]">
+                <button className="rounded-lg bg-[#ff00006b] text-sm  w-[40%] px-2 flex flex-col justify-center  items-center">
+                  {task.timeToComplete}
+                </button>
+                <button className="rounded-lg bg-[#00a2ff86] text-sm  px-2 w-[40%] flex flex-col justify-center  items-center">
+                  {task.dueDate}
+                </button>
+                <button className="rounded-lg bg-[#00ff1186] text-sm  px-2 w-[40%] flex flex-col justify-center  items-center">
+                  {task.grouping}
+                </button>
+              </div>
             </div>
-            <div className="flex flex-row w-[90%] h-[20%] justify-evenly item-center ml-[-5%] gap-[10%] mt-[10%]">
-              <button className="rounded-lg bg-[#ff00006b] text-sm  w-[40%] px-2 flex flex-col justify-center  items-center">
-                {task.timeToComplete}
-              </button>
-              <button className="rounded-lg bg-[#00a2ff86] text-sm  px-2 w-[40%] flex flex-col justify-center  items-center">
-                {task.dueDate}
-              </button>
-              <button className="rounded-lg bg-[#00ff1186] text-sm  px-2 w-[40%] flex flex-col justify-center  items-center">
-                {task.grouping}
-              </button>
-            </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </div>
   );
