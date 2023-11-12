e19f7f23be328140dc4adaaff8f0bd1ef1cde0e7
Mithun Balasubramanian <24193159+mithunb9@users.noreply.github.com>
2023-11-11 19:18:07 -0600

    update .gitignore

--- client/.next/static/chunks/webpack.js
+++ client/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0328e9a721916f19"; }
+/******/ 		__webpack_require__.h = function() { return "016a61131891ce69"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
