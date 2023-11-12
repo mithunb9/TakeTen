93c46a07311b65082d500a9f1ea3ab11387c1564
Mithun Balasubramanian <24193159+mithunb9@users.noreply.github.com>
2023-11-12 09:00:26 -0600

    update styling

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,7 +1,24 @@
+import { useEffect } from "react";
 import Navbar from "../components/NavBar";
 import KanbanBoard from "./screens/kanbanBoard";
 
 export default function Home() {
+  useEffect(() => {
+    if ("Notification" in window) {
+      Notification.requestPermission().then((permission) => {
+        new Notification("Welcome to Take10!", {
+          body: "You can now receive notifications from Take10!",
+        });
+      });
+    }
+  }, []);
+
+  function sendNotification() {
+    new Notification("Welcome to Take10!", {
+      body: "You can now receive notifications from Take10!",
+    });
+  }
+
   return (
     <>
       <div
